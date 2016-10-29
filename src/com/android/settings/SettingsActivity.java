f01448e045fde3dbe0d907ce0d7323d2893f833d
bigrushdog <randall.rushing@gmail.com>
2016-10-28 21:41:01 -0700

    DUI: Initial N checkin
    Change-Id: If8d277d124e2844810482a52bfb951549ee8c999

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -133,6 +133,10 @@ import com.android.settingslib.drawer.DashboardCategory;
 import com.android.settingslib.drawer.SettingsDrawerActivity;
 import com.android.settingslib.drawer.Tile;
 import com.flash.settings.FlashSettings;
+import com.flash.settings.fragments.FlingSettings;
+import com.flash.settings.fragments.NavbarSettings;
+import com.flash.settings.fragments.SmartbarSettings;
+import com.flash.settings.fragments.PulseSettings;
 
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -365,8 +369,12 @@ public class SettingsActivity extends SettingsDrawerActivity
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
             FlashSettings.class.getName(),
-            DisplayRotation.class.getName()
-            ExpandedDesktopPreferenceFragment.class.getName()
+            DisplayRotation.class.getName(),
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName()
     };
 
 
